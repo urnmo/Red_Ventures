6b80a1a3a0c16df98ad301a0dc15feb13e6f5140
urnmo <aaron0moore@gmail.com>
2016-11-02 12:57:40 -0400

    Initial Setup of files

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict'
+
+let gulp = require('gulp');
+let babel = require('gulp-babel');
+let sass = require('gulp-sass');
+let commentless = require('gulp-strip-css-comments');
+
+gulp.task('default', ['html', 'css','js']);
+
+gulp.task('html', function(){
+    return gulp.src('index.html')
+        .pipe (gulp.dest('public/'))
+});
+
+gulp.task('css', function(){
+    return gulp.src('style.scss')
+        .pipe(sass())
+        .pipe(commentless())
+        .pipe(gulp.dest('public/'))
+});
+
+gulp.task('js', function(){
+    return gulp.src('app.js')
+        .pipe(babel({
+            presets: ["es2015"]
+        }))
+        .pipe(gulp.dest('public/'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('index.html', ['html']);
+    gulp.watch('style.scss'), ['css'];
+    gulp.watch('app.js'), ['js'];
+})
